a280baa853979d60754d1d56d3842436902e41b9
adrielmendezrios <adriel315@gmail.com>
2025-03-01 18:09:06 -0500

    added 2nd route

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/world')
+def world():
+    return "world"
+
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
